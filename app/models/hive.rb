b656ad4a863baa9cbd897becd51d6bf38e38e9e9
merittrompeter <mktrompeter@gmail.com>
2015-07-31 07:16:02 -0700

    date pickers match

--- app/models/hive.rb
+++ app/models/hive.rb
@@ -1,7 +1,9 @@
 class Hive < ActiveRecord::Base
   belongs_to :user
-
   has_many :inspections
+
+  validates :nickname, :location, presence: true
+
 
   def hive_owner user
     user && user.id == self.user.id
--- app/models/inspection.rb
+++ app/models/inspection.rb
@@ -1,4 +1,6 @@
 class Inspection < ActiveRecord::Base
   belongs_to :hives
   # has_and_belongs_to_many :pests
+
+  validates :inspection_date, presence: true
 end
